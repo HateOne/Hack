53a207c32e995242a9eb18cee11605e358c71db8
Nikita <khramov-2016@inbox.ru>
2024-02-18 16:52:44 +0400

    Initial commit

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+
+    @Test
+    public void shouldCorrectAmount() {
+        assertEquals(service.remain(2800), 200);
+
+    }
+    @Test
+    public void shouldAmountZero() {
+        assertEquals(service.remain(0), 1000);
+    }
+
+    @Test
+    public void shouldBuyExtraIfTheAmount1000() {
+        assertEquals(service.remain(0), 1000);
+    }
+
+}
+
